9c14107440cc9aaca6c3c7e7ec0fd6aa3a4d8847
Marcin_FBI <88114702+FBI223@users.noreply.github.com>
2024-11-07 08:44:16 +0100

    8 small fix

--- zadania_5/8.hs
+++ zadania_5/8.hs
@@ -1,11 +1,25 @@
 
 eratosthenes :: Int -> [Int]
-eratosthenes n
+eratosthenes n = sito n [2..n]
+
+
+sito :: Int -> [Int] -> [Int]
+sito n  [] = []
+sito n (x:xs) -- nasz kazdy x jest liczba pierwsza bo juz kilka razy filtrowalismy od 2 do x-1 i nasz x dalej jest w liscie czyli jest liczba pierwsza
+    | x * x > n = x : xs  -- jesli po filtrowaniu doszlismy do konca to po prostu doklejasz x do koncowej listy i konczysz
+    | otherwise = x : sito n (filtruj x xs) -- usuwanie wszystkich wielokrotnosci x z listy koncowej
+-- last xs bierze aktualnie najwyzsza wartosc listy , ( moze byc ale nie musi liczba pierwsza )
+
+
+
+
+
+
+eratosthenesRek :: Int -> [Int]
+eratosthenesRek n
     | n < 2 = []
     | n == 2 = [2]
     | n > 2 = sitoRek [2..n]
-
-
 
 sitoRek :: [Int] -> [Int]
 sitoRek  [] = []
