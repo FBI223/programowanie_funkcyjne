78eaad6122e7f67adc0fe156e7cb3e13731c4da3
FBI223 <sztukowski@vp.pl>
2024-12-02 21:38:13 +0100

    15/16 kolokwium done

--- kolos_1/16/2.hs
+++ kolos_1/16/2.hs
@@ -1,2 +1,16 @@
 val :: Integer -> Integer -> Integer
-val _ _ = 0
+val a b
+  | a > 0 && b > 1 = maxVal
+  | otherwise = error "Invalid input: a must be > 0 and b must be > 1"
+  where
+    powers = takeWhile (\x -> x <= a && mod a x == 0) [b^n | n <- [0..]]
+    maxVal = if null powers then 0 else fromIntegral (length powers - 1)
+
+
+
+g :: Integer -> Integer -> [Integer]
+g k v = filter (\n -> val n k == v) [2..]
+
+
+
+
--- kolos_1/16/3.hs
+++ kolos_1/16/3.hs
@@ -2,3 +2,13 @@ data Klos a = Klos {
     poczatek :: [a],
     koniec :: [a]
 }
+
+wnpk :: Klos a -> a -> Klos a
+wnpk ( Klos poczatek koniec ) dodawany = Klos ( dodawany : poczatek ) koniec
+
+wnkk :: Klos a -> a -> Klos a
+wnkk ( Klos poczatek koniec ) dodawany = Klos  poczatek  ( dodawany : koniec )
+
+k2list :: Klos a -> [a]
+k2list ( Klos poczatek koniec ) = poczatek ++ ( reverse koniec )
+
