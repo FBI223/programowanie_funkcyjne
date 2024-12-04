877125048fef49b1187962f4f8f648dbe64751de
FBI223 <sztukowski@vp.pl>
2024-12-04 12:34:38 +0100

    added good 1 / kolokwum 1

--- kolos_1/real/1.hs
+++ kolos_1/real/1.hs
@@ -1,23 +1,10 @@
 
-
+wyplata :: Int -> Int -> Int -> [Int]
+wyplata a b kwota = [x | x <- [1..kwota], czyMozliwaWyplata a b x]
 
 
 czyMozliwaWyplata :: Int -> Int -> Int -> Bool
-czyMozliwaWyplata a b kwota = nwd a b == 1 || kwota `mod` nwd a b == 0
-
-nwd :: Int -> Int -> Int
-nwd a b 
-    | reszta_a_b /= 0 = nwd b reszta_a_b
-    | otherwise  = b
-    where 
-        reszta_a_b = mod a b
-
-
-nww :: Int -> Int -> Int
-nww a b = div (a * b) nwd_a_b
-    where
-        nwd_a_b = (nwd a b)
-
-nww_wiele :: Int -> Int -> Int -> Int
-nww_wiele a b c = nww a ( nww b c )
-
+czyMozliwaWyplata a b kwota 
+    | kwota < 0 = False 
+    | kwota == 0 = True
+    | kwota > 0 = czyMozliwaWyplata a b (kwota - a) || czyMozliwaWyplata a b (kwota - b)
