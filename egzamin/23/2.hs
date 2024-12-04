478af174fd53a5c2c87c0c6d5da55c1f8ab4ca6f
FBI223 <sztukowski@vp.pl>
2024-12-04 14:44:41 +0100

    bezpuntkowe init

--- egzamin/23/2.hs
+++ egzamin/23/2.hs
@@ -4,13 +4,29 @@
 --reverse_custom xs = foldl (\acc x -> x : acc) [] xs
 
 --reverse_custom :: [a] -> [a]
+--reverse_custom = foldl (\acc x -> (:) x acc) [] 
+
+--reverse_custom :: [a] -> [a]
+--reverse_custom = foldl (\acc x -> flip (:) acc x) [] 
+
+--reverse_custom :: [a] -> [a]
+--reverse_custom = foldl (\acc -> flip (:) acc) [] 
+
+
+reverse_custom :: [a] -> [a]
+reverse_custom = foldl ( flip (:)) [] 
+
+
+
+
+--reverse_custom :: [a] -> [a]
 --reverse_custom xs = foldl (flip (:)) [] xs
 
 --reverse_custom :: [a] -> [a]
 --reverse_custom = foldl ( flip (:) ) [] 
 
-reverse_custom :: [a] -> [a]
-reverse_custom = foldl ( flip (:) ) [] 
+--reverse_custom :: [a] -> [a]
+--reverse_custom = foldl ( flip (:) ) [] 
 
 
 conrev :: [[a]] -> [a]
@@ -22,5 +38,23 @@ conrev = concat . map reverse_custom
 --reverse_custom_foldr :: [a] -> [a]
 --reverse_custom_foldr lista = foldr (\x acc -> acc ++ [x]) [] lista
 
+
+--reverse_custom_foldr :: [a] -> [a]
+--reverse_custom_foldr lista = foldr (\x acc -> flip (++) [x] acc ) [] lista
+
+
+--reverse_custom_foldr :: [a] -> [a]
+--reverse_custom_foldr  = foldr (\x -> flip (++) [x] ) [] 
+
+--reverse_custom_foldr :: [a] -> [a]
+--reverse_custom_foldr  = foldr (\x -> flip (++) (x : []) ) [] 
+
+
+--reverse_custom_foldr :: [a] -> [a]
+--reverse_custom_foldr  = foldr (\x -> flip (++) ( (: []) x)   ) [] 
+
 reverse_custom_foldr :: [a] -> [a]
-reverse_custom_foldr = foldr (flip (++) . (: [])) []
+reverse_custom_foldr  = foldr (  flip (++) .  (: [])   ) [] 
+
+--reverse_custom_foldr :: [a] -> [a]
+--reverse_custom_foldr = foldr (flip (++) . (: [])) []
