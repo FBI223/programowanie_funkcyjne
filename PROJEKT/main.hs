dfad06d6797e6bbfab98abb0a6c45433e9b32c80
FBI223 <sztukowski@vp.pl>
2025-01-13 02:11:08 +0100

    projekt v2

--- PROJEKT/main.hs
+++ PROJEKT/main.hs
@@ -5,16 +5,95 @@ import System.Environment
 
 -- Funkcja główna programu
 
+--splitEquation "kot + kto = tok"
+--splitEquation "Send + More = MONEY"
+--splitEquation "rEBuS * i = SUDOKU"
+
+
+-- "KTO + KOT = TOK"
+-- "TRZY + TRZY = SZESC"
+-- "GRAD + DESZCZ = STRATA"
+-- "KOGUT + KURA = JAJKO"
+-- "LUK + LUK = KOLO"
+-- "CHMURA + CHMURA = DESZCZ"
+-- "KIOTO + OSAKA = TOKIO"
+-- "REBUS * I = SUDOKU"
+-- "WILK + UNIKA = LUDZI"
+--
+-- "USA + USSR = PEACE"
+-- "Send + More = MONEY"
+--
+-- "ZERO + ZERO = JEDEN"
+-- "POL + POL = CALA"
+-- "ROZUM - DUZO = MOZE"
+-- "TEST + JEST = SUPER"
+-- "DOM * DOM = MIASTO"
+-- "KWARTA + KWARTA = POLOWA"
+-- "BLAD + BLAD = GAUSS"
+-- ""
+-- ""
+-- ""
+-- ""
+-- ""
+-- ""
+-- ""
+-- ""
+-- ""
+-- ""
+-- ""
+-- ""
+-- ""
+-- ""
+-- "BUM + BUM + BUM = DUD"
+-- "OLD + OLD + OLD = GOOD"
+-- "BYE + BYE + BYE + BYE + BYE + BYE = RAY"
+-- "TED + HAS + GOOD = TASTE"
+-- "LYNNE + LOOKS = SLEEPY"
+-- "NOTICE + NICE = PRICES"
+-- "LEAH + LOVES = RUSSIA"
+-- ""
+-- ""
+-- ""
+-- ""
+-- ""
+-- ""
+
+
+
+
+
+
+allPossibilities :: String -> [[ (Char, Char) ]]
+allPossibilities linia = zipWithEach unikalneLitery permutacjeLiczb
+  where
+    unikalneLitery = uniqueCharacters linia
+    ileLiter = length unikalneLitery
+    permutacjeLiczb = kPermutations "0123456789" ileLiter
+
+
+kPermutations :: (Eq a) => [a] -> Int -> [[a]]
+kPermutations [] _ = []
+kPermutations _ 0 = [[]]
+kPermutations zbior k = [x : ys | x <- zbior, ys <- kPermutations (filter (/= x) zbior) (k - 1) ]
+
+
+zipWithEach :: [b] -> [[a]] -> [[ (b, a) ]]
+zipWithEach bs listOfLists = map (zip bs) listOfLists
+
+
+uniqueCharacters :: String -> [Char]
+uniqueCharacters [] = []
+uniqueCharacters (x:xs)
+    | toUpper x >= 'A' && toUpper x <= 'Z'     = toUpper x : uniqueCharacters (filter (\char -> toUpper char /= toUpper x) xs)
+    | otherwise                                = uniqueCharacters xs
 
 splitEquation :: String -> [String]
-splitEquation equation = words $ map (\char -> filterEquation char ) equation
+splitEquation equation = words $ map (\char -> toUpper char ) equation
 
 
-filterEquation :: Char -> Char
-filterEquation c
+toUpper :: Char -> Char
+toUpper c
   | c >= 'a' && c <= 'z' = toEnum (fromEnum c - 32)
   | c >= 'A' && c <= 'Z' = c
   | otherwise = ' '
-
-
 
