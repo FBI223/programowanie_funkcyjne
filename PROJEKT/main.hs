35bf2d9f05ba53b18f1ada9ef5b040f20aa28d98
FBI223 <sztukowski@vp.pl>
2025-01-14 03:24:41 +0100

    Project Completed - dokumentacja

--- PROJEKT/main.hs
+++ PROJEKT/main.hs
@@ -4,48 +4,31 @@ import System.Environment; -- dla funkcji getArgs
 -- :set args "C:\\Users\\msztu\\Documents\\haskell_projects\\PF\\programowanie_funkcyjne\\PROJEKT\\mystery"
 
 
-main :: IO ()
-main = do
+main :: IO () 
+main = do 
 
-    contents <- readFile "in"
+    contents <- readFile "in2" -- wczytywanie zawartosci pliku do zmiennej contents
     
-    let linie = lines contents
+    let linie = lines contents -- rozdziela caly tekst na liste wierszy
     
-    let wyniki = map cryptharithmsSolver linie
+    let wyniki = map cryptharithmsSolver linie -- przetwarzanie kazdej linii poprzez map
     
-    writeFile "out" (unlines (map show wyniki))
+    writeFile "out2" (unlines (map show wyniki)) -- zapisanie do pliku ciagi znakow
     
-    mapM_ (putStrLn . show) wyniki
+    mapM_ (putStrLn . show) wyniki -- wypisanie wynikow na ekran
 
 
-
-
-zipWithEach :: [b] -> [[a]] -> [[ (b, a) ]]
-zipWithEach bs listOfLists = map (zip bs) listOfLists
-
-
-allPossibilities :: String -> [[ (Char, Char) ]]
-allPossibilities linia = wszystkie_mozliwosci_przypisan
-  where
-    unikalneLitery = uniqueCharacters linia
-
-    ileLiter = length unikalneLitery
-
-    permutacjeLiczb 
-              |   ileLiter < 10   = knPermutations "0123456789" ileLiter
-              |        otherwise  = permutations   "0123456789"
-    
-    wszystkie_mozliwosci_przypisan = zipWithEach unikalneLitery permutacjeLiczb
-
-
+-- glowna funkcja
+-- zakladamy ze tylko jeden typ operacji wystepuje w danym rownaniu
+-- jesl istnieje rozwiazanie to cryptharithmRecursive zwroci pierwsze napotkane rozwiazanie a jak nie ma to zwroci []
 cryptharithmsSolver :: String -> [Int]
 cryptharithmsSolver linia = wynik
   where
-    wszystkie_dopasowania = allPossibilities linia
-    rozbite_zdanie = splitEquation linia 
+    wszystkie_dopasowania = allPossibilities linia -- wszystkie mozliwe mini-slowniki dopasowan cyfr do liter
+    rozbite_zdanie = splitEquation linia  -- rozbite zdanie na liste stringow
     operacja 
-        | elem '*' linia = mult
-        | elem '-' linia = sub
+        | elem '*' linia = mult -- jesli jest znaczek * to znaczy ze mamy sprawdzic mnozenie 
+        | elem '-' linia = sub  -- zakladamy ze tylko jeden typ operacji wystepuje w danym rownaniu
         | elem '+' linia = add
         |otherwise = add
   
@@ -53,42 +36,48 @@ cryptharithmsSolver linia = wynik
 
 
 
-
+-- dla kazdego zestawu/mozliwosci przypisan cyfr do liter oraz dla jednego zdania ( zestawu slow)
+-- sprawdzamy czy nasze rownanie LEWA == PRAWA po konwersji na liczby
 cryptharithmRecursive :: [[ (Char, Char) ]] -> (Int -> Int -> Int) -> [String] -> [Int]
 cryptharithmRecursive [] _ _ = []
 cryptharithmRecursive _ _ [] = []
-cryptharithmRecursive (mini_slownik : reszta_slownika) operacja zdanie
-                    | checkEquation equation operacja  =  equation
-                    | otherwise               =  cryptharithmRecursive reszta_slownika operacja zdanie
+cryptharithmRecursive (mini_slownik : reszta_slownika) operacja zdanie -- dla kazdego slownika
+                    | checkEquation equation operacja  =  equation -- jesli sprawdzilismy nasze rownanie z operacja daje LEWA = PRAWA 
+                    | otherwise               =  cryptharithmRecursive reszta_slownika operacja zdanie -- szukamy dalej z innym slownikiem przypisan cyfr do liter
   where
     equation = convertEquation $ changeSentenceIntoEquation zdanie mini_slownik
+    -- zmieniamy liste slowa na liste liczb oraz konwertujemy ta liste na faktyczne Int
 
 
+
+-- sprawdzamy czy nasze rownanie LEWA == PRAWA , dla podanej operacji arytmetycznej
 
 checkEquation :: [Int] -> (Int -> Int -> Int) -> Bool
-checkEquation [] _ = False
-checkEquation [_] _ = False
+checkEquation [] _ = False -- pomin
+checkEquation [_] _ = False -- pomin
 checkEquation xs op = foldl op (head initList) (tail initList) == last xs
-  where
-    initList = init xs 
+  where -- wykonujemy operacje od lewej do prawej , ale bez ostatniego elementu listy bo to nasz wynik operacji
+    initList = init xs -- lista bez ostatniego elementu
 
 
-
+ -- kowertujemy wszystkie liczby w postaci string w equation na int
 convertEquation :: [String] -> [Int]
-convertEquation strs
-  | all isValidStringNumber strs = map stringToInt strs
-  | otherwise              = []
+convertEquation strs -- jesli wszystkie liczby nie maja 0 na poczatku albo maja jakiekolwiek cyfry
+  | all isValidStringNumber strs = map stringToInt strs  --  to konwertujemy wszystkie liczby
+  | otherwise              = [] -- gdy chociaz jedna liczba nie jest ok to pomijamy ten przypadek rownania
 
 
-
+-- dokonujemy filtrakcji ktora liczba jest dozwolona w kryptarytmie np 
+-- np dla slowa "ABC" niedozwolone sa np 012 
 isValidStringNumber :: String -> Bool
 isValidStringNumber num
-    | null num           = False
-    | length num == 1    = True  
-    | head num == '0'    = False 
-    | otherwise          = True
+    | null num           = False -- pusty string
+    | length num == 1    = True  -- cyfra jest dozwolona
+    | head num == '0'    = False -- jesli liczba zaczyna sie od zera to nie jest ok
+    | otherwise          = True  -- inne przypadki jest ok 
 
 
+-- dozwolone operacje arytmetyczne
 add :: Int -> Int -> Int
 add x y = x + y
 
@@ -99,57 +88,90 @@ mult :: Int -> Int -> Int
 mult x y = x * y
 
 
+-- konwersja z string na liczbe
 stringToInt :: String -> Int
-stringToInt [] = 0
-stringToInt str
-    | all isDigit str = foldl (\acc x -> acc * 10 + charToDigit x) 0 str
-    | otherwise = 0
-  where
-    isDigit c = c >= '0' && c <= '9'
-    charToDigit c = fromEnum c - fromEnum '0'
+stringToInt [] = 0 -- baza rekurencji
+stringToInt str = foldl (\acc x -> acc * 10 + charToDigit x) 0 str
+  where -- jesli caly string sklada sie z cyfr to konwertujemy z uzyciem foldl i zwiekszaniem potegi 10 , 10^n
+    charToDigit c = fromEnum c - fromEnum '0' -- prosta konwersja '9' na 9 (char na int)
+-- fromEnum zwraca pozycje z enumeration type czyli dla fromEmum '0' zwroci 30
 
+
+-- zmieniamy wszystkie slowa w zdaniu na liczby
 changeSentenceIntoEquation :: [String] -> [ (Char, Char) ] -> [String]
 changeSentenceIntoEquation [] _ = [] 
 changeSentenceIntoEquation (x:xs) slownik = changeWordIntoNumber x slownik : changeSentenceIntoEquation xs slownik
 
 
+-- zmieniamy nasze slowo w ciag cyfr czyli liczbe za pomoca slownika przypisan liter do cyfr
 changeWordIntoNumber :: String -> [ (Char, Char) ] -> String
 changeWordIntoNumber [] _ = [] 
 changeWordIntoNumber (x:xs) slownik = changeLetterIntoDigit x slownik : changeWordIntoNumber xs slownik
 
+-- dostajemy na wejsciu litere i zmieniamy ta litere wedlug naszego slownika przypisan cyfr do liter
 changeLetterIntoDigit :: Char ->  [ (Char, Char) ] -> Char
-changeLetterIntoDigit c slownik = snd $ head $ filter (\(a,b) -> a == c) slownik
+changeLetterIntoDigit c slownik = snd $ head $ filter (\(a,b) -> a == c) slownik -- szukamy instrukcji przypisania dla naszej litery
+                 -- head wyciagamy [(A,1)] a potem wyciagamy cyfre z krotki ( klucz wartosc ) = ( litera cyfra ) i zwracamy 
+
+
+-- wczytuje linie z rownaniem i daje na wyjscie wszystkie mozliwe przypisania
+-- przypisania cyfr do liter np jak mamy "A+B=C"
+-- to dostajemy [ [(A,0),(B,1),(C,2)] ...  [(A,9),(B,8),(C,7)] ]
+allPossibilities :: String -> [[ (Char, Char) ]]
+allPossibilities linia = zipWithEach unikalneLitery permutacjeLiczb
   where
-    przypisanie = filter (\(a,b) -> a == c) slownik
+    unikalneLitery = uniqueCharacters linia -- np "ABC"
+    ileLiter = length unikalneLitery
+    permutacjeLiczb --np unikalne litery == "ABC" to permutacje [ "012" ... "789"]
+              |   ileLiter < 10   = knPermutations "0123456789" ileLiter
+              |        otherwise  = permutations   "0123456789"
+    
 
 
+-- zipuje unikalne litery [b] z wszystkimi permutacjami cyfr ( n liter oraz n cyfr)
+-- tworzy to  wszystkie mozliwosci przypisan np [ [(A,1),(B,0)] ... [(A,9),(B,8)] ]
+zipWithEach :: [b] -> [[a]] -> [[ (b, a) ]]
+zipWithEach bs listOfLists = map (zip bs) listOfLists
+
+-- permutacje calego zbioru
 permutations :: [Char] -> [[Char]]
 permutations [] = [[]] 
 permutations zbior = [x : ys | x <- zbior, ys <- permutations (filter (/= x) zbior)]
 
 
-
+-- permutacje czesci zbioru np wybieram permutacje k-elementow z n-zbioru
+-- np 2-permutacja z 10-zbioru to [[0,1] .. [0,9] .. [9,8]] 
+-- wyjdzie nam 10!/2! permutacji
 knPermutations :: [Char] -> Int -> [[Char]]
 knPermutations [] _ = []
 knPermutations _ 0 = [[]]
 knPermutations zbior k = [x : ys | x <- zbior, ys <- knPermutations (filter (/= x) zbior) (k - 1)]
 
-
--- [x : ys | x <- zbior, x /= '0' || null ys, ys <- knPermutations (filter (/= x) zbior) (k - 1)]
-
-
+-- szukamy jakie sa unikalne znaki alfabetu
+-- np "AB ABA BABA C" da nam "ABC"
+--
+-- jesli znak przefiltrowany przez toUpper jest znakiem alfabetu to dolacz go do wyniku oraz przefiltruj 
+-- oraz przefiltruj pozostaly string z danej litery i usun ja z tego stringa
+-- idz dalej z rekurencja , jak napotkasz inny znak niz alfabetu to pomin
 uniqueCharacters :: String -> [Char]
 uniqueCharacters [] = []
-uniqueCharacters (x:xs)
-    | toUpper x >= 'A' && toUpper x <= 'Z'     = toUpper x : uniqueCharacters (filter (\char -> toUpper char /= toUpper x) xs)
+uniqueCharacters (x:xs) -- iteracyjnie przechodzimy po kolejnych znakach 
+    | toUpper x >= 'A' && toUpper x <= 'Z'     = toUpper x : uniqueCharacters (filter (\char -> toUpper char /= toUpper x) xs) 
     | otherwise                                = uniqueCharacters xs
 
+
+
+
+-- dzieli wejsciowy string rownania na operandy oraz wynik
+-- words rozbija nam string rozdzielony spacjami na tablice stringow bez bialych znakow pomiedzy slowami
+-- mozemy tak zrobic bo kazdy inny znak niz alfabetu to zostanie przeksztalcony
+-- przeksztalcony w spacje przez toUpper
 splitEquation :: String -> [String]
 splitEquation equation = words $ map (\char -> toUpper char ) equation
 
 
 toUpper :: Char -> Char
-toUpper c
-  | c >= 'a' && c <= 'z' = toEnum (fromEnum c - 32)
-  | c >= 'A' && c <= 'Z' = c
-  | otherwise = ' '  
+toUpper c -- to enum przesuwa znak ASCII o 32 i potem konweruje na char spowrotem
+  | c >= 'a' && c <= 'z' = toEnum (fromEnum c - 32) -- litery musza byc wielkie w algorytmie
+  | c >= 'A' && c <= 'Z' = c -- zostawiamy jak jest 
+  | otherwise = ' '  -- napotkalismy inny znak niz alfabet to pomijamy
